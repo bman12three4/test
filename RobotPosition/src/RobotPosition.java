4a8e83c1ec38c2cdcd175690b1cd2400f2bd56a0
bman12three4 <byron.lathi@student.lovett.org>
2019-01-09 12:39:10 -0500

    Add main window

--- RobotPosition/src/RobotPosition.java
+++ RobotPosition/src/RobotPosition.java
@@ -1,8 +1,47 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 
 public class RobotPosition {
 
+	
+	private JFrame frame;
+	
 	public static void main(String[] args) {
-		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RobotPosition window = new RobotPosition();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public RobotPosition() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+
+		/**
+		 * Create the main window with an absolute layout.
+		 */
+		frame = new JFrame();
+		frame.setSize(400, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setLocationRelativeTo(null);
+
 	}
 
 }
